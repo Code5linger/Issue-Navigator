a5cb5959c52f427300aaccda4a6bf6c83c8db446
Code5linger <sakib.ahmed.1708@gmail.com>
2024-09-19 08:41:51 +0600

    Implementing the API

--- app/issues/_componenets/IssueForm.tsx
+++ app/issues/_componenets/IssueForm.tsx
@@ -2,6 +2,7 @@
 
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
+// import { issueSchema } from '@/app/validationSchemas';
 import issueSchema from '@/app/validationSchemas';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Callout, TextField } from '@radix-ui/themes';
